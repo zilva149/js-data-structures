3b78c05ded810fcdc80f17caf235f9f1720de4d4
zilva149 <114057257+zilva149@users.noreply.github.com>
2023-04-10 12:41:57 +0300

    initial binary search tree commit

---
+++ js/BinarySearchTree.js
@@ -0,0 +1,13 @@
+export class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {}
+
+  lookup(value) {}
+
+  remove(value) {}
+
+  traverse() {}
+}
--- js/app.js
+++ js/app.js
@@ -5,15 +5,13 @@ import { HashTable } from "./HashTable.js";
 import { LinkedList } from "./LinkedList.js";
 import { DoublyLinkedList } from "./DoublyLinkedList.js";
 import { Queue } from "./Queue.js";
+import { BinarySearchTree } from "./BinarySearchTree.js";
 
 const arr = new MyArray();
 const hashTable = new HashTable(100);
 const linkedList = new LinkedList(0);
 const doublyLinkedList = new DoublyLinkedList(0);
 const myQueue = new Queue();
+const binarySearchTree = new BinarySearchTree();
 
-myQueue.enqueue("Zilva");
-myQueue.enqueue("Monika");
-myQueue.enqueue("Tomas");
-console.log(myQueue.lookup(2));
-console.log(myQueue);
+console.log(binarySearchTree);
